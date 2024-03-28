eb200535158f2ae12889f49c4471ab37f2db3108
veer300 <153541760+veer300@users.noreply.github.com>
2024-03-28 10:06:08 +0530

    Add files via upload

--- arrow.js
+++ arrow.js
@@ -1,71 +1,31 @@
-/* Arrow fuction is like a syntactical suger:
-wrtting less getting more that is why we are makinguse of arroe funtion:
-write less and do more :
-in arrow->there is no function keyword:
- right  side  you have paramenter:
- */
-
-  /* function add(a,b){
-    console.log("Inside the add function:");
-    return a+b;
-   }*/
-  /* Arrow function  */
- /*  const add1=(a,b)=>{
-    console.log("Inside the add function:");
-    return a+b;
-
-  }
-
-
-  console.log(add(5,7));
-  console.log(add1(5,7));
-
-  const hello=()=>{
-    console.log("Hello students:");
-
-  }
-
- hello();
-
-  */
-
- // function with mmultiple arguments and return type:
-//    sum_Of_Three=(a,b,c)=>{
-//     console.log("Insude sum three:");
-//      return a+b+c;
-//    }
-
-//    console.log(sum_Of_Three(4,5,6));
-//    // function with no  returns:
-//     greet=(name)=>{
-//         console.log("Hello "+name);
-//     }
-
-//     greet("Hello Mr veer")
+/* Intention of arrow function :Write less get more: */
 /* 
-  sum=(a,b)=>  a+b;// you don't need th use of return 
+function add(a, b) {
+  console.log("Inside the add function");
+  return a + b;
+}
+console.log(add(5,7));
+ */ /* In arrow function there is no function keyword : */
+/*  const add1=(a,b)=>{
+    console.log('Inside arrow function :');
+   return a+b;
+}
+
+console.log(add1(5,7));
+
+ const greet=()=>{
+    console.log('Hello folks :');
+}
 
 
-  console.log(sum(4,6)); */
-  /* Limitation of arrow function:
-  1: They can  access the argument object 
-  2:No prtotype property
-  3:Can't be used with new property
-  4:can't we use as constructors
+ greet(); */
 
-  
-  
-  */
+ sum=(a,b)=> {return a+b;}
 
-  /* Revsion:
-  function is a block of code
-  2:Resuablity
-  write one use many 
+ console.log(sum(55,7));
+ /* Even or odd: */
 
-   */
-
-  evenOrOdd=(n)=>{// aroe +ternary operaotor:
-   return (n%2==0) ?"even":"odd";
-  }
-
-  console.log(evenOrOdd(6));
+ evenOrOdd=(n)=>{
+return (n%2==0)?'Even':'odd';
+ }
+ console.log(evenOrOdd(8));
--- lecture1.js
+++ lecture1.js
@@ -1,50 +1,102 @@
+/* Writting the logic and reusing multiple time that typically called as function */
+/* Function always reduces redundancy: */
+/* While creating function we always use a prefix called as function : */
+
+/* function greet_students() {
+    
+console.log('\nHi');
+console.log('Welocome to the 7 PM class :');
+console.log('Keep learning ');
+}
+
+greet_students(); */
 /* 
-let  arr_nums=[1,2,3,4,5];
-console.log(arr_nums);
-console.log(arr_nums[0]);
-console.log(arr_nums[1]);
-console.log(arr_nums[9]);
-console.log(arr_nums.length);
- */
+function Sum(a,b) {  //parameter -?dummy variable :
+    console.log('Inside the sum sunction:');
+    return a+b;
+}
 
-/* Other way of define an array: */
-/* arr=Array(1,2,3,4,5,6)
-console.log(arr);
 
-const empty_arr=Array(5);
+console.log(Sum(5,6));// Argument  */
+/* Factorial of a number  */
 
- */
-/* fruit =new Array ('Apple','Mango','Banana','Grapes');
- console.log(fruit);
+/* function fact(n) {
+        if(n==0||n==1) return  1;
+    result=1;
+    for(i=n;i>=1;i--){
+        result*=i;
+    }
+    return result;
 
- <1 empty item>// not looking good we will  not gone to use delete longer period of time:
+    
+}
 
-  */
-/*  arr=[1,2,3,4,5]
-   console.log(arr);
-   //delete arr[2];
-   console.log(arr);
+ result=fact(6)
+ console.log(result); */
 
-    for (let i = 0; i < arr.length; i++) {
-         process.stdout.write(arr[i]+" ")
+ /*Function in javaScript is called first called citizen":
+ A fuunction could be assiggn to a variable:
+ you can pass function as an argument:
+ function can be returned from a function itself :  
+*/
+/* when you  assign  function to the variable :name of the function will  be changed and new name  is the name of varible  which you assign it  :
+  when you console the function call it will return undefined if function is not returning any value:*/
+/* hello_func=function (a,b) {//  assign function to a varibale :
+    console.log('Hello Students:');
+    return  a+b;
+}
+
+hello_func();
+console.log(hello_func(5,6)); */
+/* Java script says that when you assignfunction to a variable no need to write even function name: */
+
+/* PAssing function as an  argument */
+
+/* function operate(a,b,fn) {
+
+    return fn(a,b);
+    
+}
+
+function sum(a,b) {
+    return a+b;
+}
+
+console.log(operate(4,6,sum)); */
+
+/* function return_greet() {
+    return function () {
+        console.log('Hello students ');
+
+        return function (a,b) {
+            return a+b;
+            
+        }
+    }
+    
+}
+
+console.log(return_greet()()(5,6));
+    return_greet()() */
+
+/* Since java Script is a silent language java script get the work done :  */
+   /*  function sum(a,b) {
+        return a+b;
         
     }
-
- */
-/* 
-sumofArr = (arr) => {
-  sum = 0;
-  for (i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum;
-};
-
-console.log("Sum of elements of an array: " + sumofArr([1, 2, 34, 5]));
+    console.log(sum(5,7,9));
  */
 
-arr=[4,6,3,6,3]
-/* For  each loop: Iteration over array elements directly :*/
-for(n of arr){
-    console.log(n);
-}
+    /* You can pass any no of argument in function without any parameter and you will be able to use that : */
+/* This is a sum function which can add any no of arguments  : */
+/* Argument is a global object avaliable to  all the function: */
+   /*  function sum() {
+        console.log(arguments);
+        sum=0;
+        for(i=0;i<arguments.length;i++){
+            sum+=arguments[i];
+        }
+        return sum;
+    }
+    console.log('Sum of all the arguments is :'+sum(5,7,6,8));
+ */
