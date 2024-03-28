95968ac91fd926c1ab81fddcb3a7f44f6d2685e6
veer300 <153541760+veer300@users.noreply.github.com>
2024-03-28 10:05:13 +0530

    Add files via upload

--- arrow.js
+++ arrow.js
@@ -1,36 +1,71 @@
+/* Arrow fuction is like a syntactical suger:
+wrtting less getting more that is why we are makinguse of arroe funtion:
+write less and do more :
+in arrow->there is no function keyword:
+ right  side  you have paramenter:
+ */
 
-/* unction add(a,b) {
-    console.log('Inside the add function :');
+  /* function add(a,b){
+    console.log("Inside the add function:");
     return a+b;
-    
-}
-
-const add1=(a,b)=>{
-    console.log('Inside add funcction  ');
+   }*/
+  /* Arrow function  */
+ /*  const add1=(a,b)=>{
+    console.log("Inside the add function:");
     return a+b;
-}
 
-console.log(add(4,5));
-console.log(add(195,7)); */
+  }
 
 
-const greet=()=>{
-    console.log('Hello veeresh');
-}
+  console.log(add(5,7));
+  console.log(add1(5,7));
 
-const add=(a,b)=> {
-    return a+b;
-}
+  const hello=()=>{
+    console.log("Hello students:");
 
-   console.log(add(6,7));
+  }
 
- greet();// ggood you  have done  well:
+ hello();
+
+  */
+
+ // function with mmultiple arguments and return type:
+//    sum_Of_Three=(a,b,c)=>{
+//     console.log("Insude sum three:");
+//      return a+b+c;
+//    }
+
+//    console.log(sum_Of_Three(4,5,6));
+//    // function with no  returns:
+//     greet=(name)=>{
+//         console.log("Hello "+name);
+//     }
+
+//     greet("Hello Mr veer")
+/* 
+  sum=(a,b)=>  a+b;// you don't need th use of return 
 
 
-const evenOrOdd=(n)=>{
-       (n%2==0)? console.log('No is even'):console.log('No is  odd');
-} 
+  console.log(sum(4,6)); */
+  /* Limitation of arrow function:
+  1: They can  access the argument object 
+  2:No prtotype property
+  3:Can't be used with new property
+  4:can't we use as constructors
 
-evenOrOdd(5);
+  
+  
+  */
 
-   
+  /* Revsion:
+  function is a block of code
+  2:Resuablity
+  write one use many 
+
+   */
+
+  evenOrOdd=(n)=>{// aroe +ternary operaotor:
+   return (n%2==0) ?"even":"odd";
+  }
+
+  console.log(evenOrOdd(6));
